f9ccd3644ce68888fe3a17f10cc46ba0082b71c4
havoXD <alangenov91@gmail.com>
2020-02-29 16:07:31 +0100

    29-02

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 /*
- * Discord Bot Builder Bot
- * Version 1.2.0
- * Robert Borghese
+ * Scav Homie
+ * Version 1.0.1
+ * havoX#7808
  */
 
 const Files = require(require('path').join(__dirname, 'js', 'Main.js')).Files;
