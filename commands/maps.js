7bf592fe31a16a997b4f52d14132ce0df09603cb
Alan Genov <alangenov91@gmail.com>
2020-08-16 01:28:38 +0200

    mapsrework

--- commands/maps.js
+++ commands/maps.js
@@ -19,8 +19,8 @@ module.exports = {
                 url: "https://gamepedia.cursecdn.com/escapefromtarkov_gamepedia/thumb/4/43/Map.png/687px-Map.png?version=bfb5aa326fd22f7f1088797294e21979"
             },
             fields: [{
-                name: 'Type "%(mapname)" to see commands for each area',
-                value: "Every area in game has different type of maps that could be useful for different things.\nScav Homie has picked a number of useful maps for each area that he thinks are useful.\n\nFactory: Detailed map\nWoods: Detailed map, hidden stashes guide and a interactive map\nCustoms: Detailed map, hidden stashes guide, interactive map and a dorms map.\nShoreline: Detailed map, hidden stashes guide, key spawns map and a resort map.\nInterchange: Detailed map and a hidden stashes guide.\nThe Lab: Detailed map and maps of each floor.\nReserve: Detailed map, Detailed map with key guide, underground radio station and D-2 Bunker map. "
+                name: 'Type "%(mapname)"',
+                value: "Every area in game has different type of maps that could be useful for different things.\nScav Homie has picked a number of useful maps for each area that he thinks are useful.\n\n```Factory:\nDetailed map\n\nWoods:\nDetailed map\nHidden stashes guide\nInteractive map\n\nCustoms:\nDetailed map\nHidden stashes guide\nInteractive map\nDorms map\n\nShoreline:\nDetailed map\nHidden stashes guide\nKey spawns map\nResort map\n\nInterchange:\nDetailed map\nHidden stashes guide\n\nThe Lab:\nDetailed map\nMaps of each floor\n\nReserve: Detailed map\nDetailed map with key guide\nUnderground radio station\nD-2 Bunker map``` "
             }, ],
             footer: {
                 text: (config.gameUpdate),
